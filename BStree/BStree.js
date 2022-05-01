cfac0460e3715b95fff498564251ccefab9b91e7
Yukkku <76182771+Yukkku@users.noreply.github.com>
2022-05-02 04:30:51 +0900

    Update BStree.js

--- BStree/BStree.js
+++ BStree/BStree.js
@@ -26,6 +26,16 @@ class BStree{
       return true
     }
   }
+  getnth(n){
+    if(this.h){
+      let f=this.s.length
+      if(n==f){
+        return this.r
+      }else{
+        return n>f?this.l.getnth(s-f-1):this.s.getnth(n)
+      }
+    }
+  }
   indexOf(n){
     if(this.h){
       if(n==this.r){
@@ -42,7 +52,31 @@ class BStree{
     }
   }
   delete(n){
-    
+    if(this.h){
+      if(this.r==n){
+        if(this.h==1){
+          delete this.r
+          delete this.l
+          delete this.s
+          this.h=0
+          this.length=0
+          return true
+        }else if(this.l.h){
+          let f=this.l.getnth(0)
+          this.l.delete(f)
+          this.r=f
+          this.set()
+          return true
+        }else{
+          this.r=this.s.r
+          this.h=1
+          this.length=1
+          this.s=new BStree()
+        }
+      }else{
+        (n>this.r?this.l:this.s).delete(n) && this.set()
+      }
+    }else return false
   }
   set(){
     if(this.h){
@@ -52,12 +86,32 @@ class BStree{
             ns=new BStree()
         if(f==2){
           ns.s=this.s
+          ns.r=this.r
           if(this.l.s.h>this.l.l.h){
+            ns.l=this.l.s.s
+            nl.l=this.l.l
+            nl.r=this.l.r
+            nl.s=this.l.s.l
+            this.r=this.l.s.r
           }else{
-            
+            ns.l=this.l.s
+            nl=this.l.l
+            this.r=this.l.r
           }
         }else{
           nl.l=this.l
+          nl.r=this.r
+          if(this.s.l.h>this.s.s.h){
+            nl.s=this.s.l.l
+            ns.s=this.s.s
+            ns.r=this.s.r
+            ns.l=this.s.l.s
+            this.r=this.s.l.r
+          }else{
+            nl.s=this.s.l
+            ns=this.s.s
+            this.r=this.s.r
+          }
         }
         ns.h=Math.max(ns.l.h,ns.s.h)+1
         ns.length=ns.l.length+ns.s.length+1
@@ -70,9 +124,4 @@ class BStree{
       this.length=this.l.length+this.s.length+1
     }
   }
-}
-
-let foo=new BStree()
-for(let i=0;i<100;i++){
-  foo.add(Math.random())
 }
