55caa12d9136424ae08d8f3b0bfa095bf745558d
Yukkku <76182771+Yukkku@users.noreply.github.com>
2022-05-22 08:28:24 +0900

    Create Heap.js

---
+++ Heap/Heap.js
@@ -0,0 +1,35 @@
+class Heap{
+  constructor(){
+    this.length=0
+  }
+  add(n){
+    if(this.length){
+      if(this.min>n){
+        [n,this.min]=[this.min,n]
+      }
+      if(this.r.length>this.l.length){
+        this.l.add(n)
+      }else{
+        this.r.add(n)
+      }
+    }else{
+      this.length=1
+      this.min=n
+      this.r=new Heap()
+      this.l=new Heap()
+    }
+  }
+  popmin(){
+    if(this.length){
+      let m=this.min
+      if(this.r.length>this.l.length){
+        this.min=this.r.popmin()
+      }else{
+        this.min=this.l.popmin()
+      }
+      return m
+    }else{
+      return
+    }
+  }
+}
